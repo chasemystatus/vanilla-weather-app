aae1db8e365061c20a51fdf80e7d34f726e744a8
chasemystatus <hayleywalden560@hotmail.com>
2025-12-20 10:49:14 +0000

    Replace hardcoded Lisbon with dynamic city query

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+function searchCity(city) {
+  // make api call and update the interface
+  let apiKey = "f8ac3ab5b2f666adta3f1e4o43e6107c";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
+  console.log("submit fired");
   event.preventDefault();
 
   let searchInput = document.querySelector("#city-input");
@@ -6,6 +14,8 @@ function handleSearchSubmit(event) {
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
