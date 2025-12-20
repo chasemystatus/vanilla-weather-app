7a852b2c8ceb58f0a34024d8c546b38f52694a5b
chasemystatus <hayleywalden560@hotmail.com>
2025-12-20 13:08:20 +0000

    Add error handling for weather API requests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 function refreshWeather(response) {
-  console.log(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
   let apiKey = "f8ac3ab5b2f666adta3f1e4o43e6107c";
   let encodedCity = encodeURIComponent(city.trim());
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${encodedCity}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
+
+  axios
+    .get(apiUrl)
+    .then(refreshWeather)
+    .catch(function (error) {
+      console.log("Something went wrong:", error);
+    });
 }
 
 function handleSearchSubmit(event) {
@@ -14,13 +26,11 @@ function handleSearchSubmit(event) {
 
   let searchInput = document.querySelector("#city-input");
 
-  let cityElement = document.querySelector("#city");
-
-  cityElement.innerHTML = searchInput.value;
-
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
